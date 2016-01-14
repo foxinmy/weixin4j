efa08c3bf4a948d6187708a72e6a21489d6da323
jinyu <foxinmy@gmail.com>
2016-01-14 12:03:21 +0800

    cleanup with findbugs

--- weixin4j-base/src/main/java/com/foxinmy/weixin4j/http/factory/Netty4HttpClient.java
+++ weixin4j-base/src/main/java/com/foxinmy/weixin4j/http/factory/Netty4HttpClient.java
@@ -107,7 +107,8 @@ public class Netty4HttpClient extends AbstractHttpClient {
 					}
 				}
 			};
-			InetSocketAddress address = useProxy ? (InetSocketAddress) params.getProxy().address() : new InetSocketAddress(
+			InetSocketAddress address = useProxy ? (InetSocketAddress) params
+					.getProxy().address() : new InetSocketAddress(
 					InetAddress.getByName(uri.getHost()), getPort(uri));
 			bootstrap.connect(address).syncUninterruptibly()
 					.addListener(listener);
@@ -188,6 +189,7 @@ public class Netty4HttpClient extends AbstractHttpClient {
 			Entry<String, List<String>> header = headerIterator.next();
 			uriRequest.headers().set(header.getKey(), header.getValue());
 		}
+		uriRequest.headers().set(HttpHeaders.ACCEPT_CHARSET, "utf-8");
 		uriRequest.headers().set(HttpHeaders.CONNECTION,
 				io.netty.handler.codec.http.HttpHeaders.Values.CLOSE);
 		return uriRequest;
