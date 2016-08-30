822117cb33f92ad83daaa2c1090038753a6282e2
jinyu <foxinmy@gmail.com>
2016-08-30 18:56:23 +0800

    md

--- weixin4j-mp/src/main/java/com/foxinmy/weixin4j/mp/api/CardApi.java
+++ weixin4j-mp/src/main/java/com/foxinmy/weixin4j/mp/api/CardApi.java
@@ -19,7 +19,7 @@ import com.foxinmy.weixin4j.util.IOUtil;
 
 /**
  * 卡券API
- * 
+ *
  * @className CardApi
  * @author jinyu(foxinmy@gmail.com)
  * @date 2016年8月3日
@@ -37,13 +37,13 @@ public class CardApi extends MpApi {
 	/**
 	 * 创建卡券:创建卡券接口是微信卡券的基础接口，用于创建一类新的卡券，获取card_id，创建成功并通过审核后，
 	 * 商家可以通过文档提供的其他接口将卡券下发给用户，每次成功领取，库存数量相应扣除。
-	 * 
+	 *
 	 * <li>1.需自定义Code码的商家必须在创建卡券时候，设定use_custom_code为true，且在调用投放卡券接口时填入指定的Code码。
 	 * 指定OpenID同理。特别注意：在公众平台创建的卡券均为非自定义Code类型。 <li>
 	 * 2.can_share字段指领取卡券原生页面是否可分享，建议指定Code码、指定OpenID等强限制条件的卡券填写false。 <li>
 	 * 3.创建成功后该卡券会自动提交审核
 	 * ，审核结果将通过事件通知商户。开发者可调用设置白名单接口设置用户白名单，领取未通过审核的卡券，测试整个卡券的使用流程。
-	 * 
+	 *
 	 * @param cardCoupon
 	 *            卡券对象
 	 * @see <a
@@ -61,7 +61,6 @@ public class CardApi extends MpApi {
 		content.put(cardType.toLowerCase(), cardCoupon);
 		JSONObject card = new JSONObject();
 		card.put("card", content);
-		System.err.println(card);
 		Token token = tokenManager.getCache();
 		String card_create_uri = getRequestUri("card_create_uri");
 		WeixinResponse response = weixinExecutor.post(
@@ -73,7 +72,7 @@ public class CardApi extends MpApi {
 	/**
 	 * 设置卡券买单：创建卡券之后，开发者可以通过设置微信买单接口设置该card_id支持微信买单功能。值得开发者注意的是，
 	 * 设置买单的card_id必须已经配置了门店，否则会报错。
-	 * 
+	 *
 	 * @param cardId
 	 *            卡券ID
 	 * @param isOpen
@@ -98,7 +97,7 @@ public class CardApi extends MpApi {
 	/**
 	 * 设置自助核销:创建卡券之后，开发者可以通过设置微信买单接口设置该card_id支持自助核销功能。值得开发者注意的是，
 	 * 设置自助核销的card_id必须已经配置了门店，否则会报错。
-	 * 
+	 *
 	 * @param cardId
 	 *            卡券ID
 	 * @param isOpen
@@ -122,7 +121,7 @@ public class CardApi extends MpApi {
 
 	/**
 	 * 创建卡券二维码： 开发者可调用该接口生成一张卡券二维码供用户扫码后添加卡券到卡包。
-	 * 
+	 *
 	 * @param expireSeconds
 	 *            指定二维码的有效时间，范围是60 ~ 1800秒。不填默认为365天有效
 	 * @param cardQRs
