3f5412bd838c4f5ebf4646dafb75d1f8b18fcaa2
jinyu <foxinmy@gmail.com>
2016-09-20 18:22:36 +0800

    修复apache的httpclient证书无效bug

--- weixin4j-qy/src/main/java/com/foxinmy/weixin4j/qy/api/NotifyApi.java
+++ weixin4j-qy/src/main/java/com/foxinmy/weixin4j/qy/api/NotifyApi.java
@@ -56,7 +56,7 @@ public class NotifyApi extends QyApi {
 	 *
 	 * @param message
 	 *            消息对象
-	 * @return 如果无权限，则本次发送失败；如果收件人不存在或未关注，发送仍然执行。两种情况下均返回无效的部分</br> { "errcode":
+	 * @return 如果无权限或收件人不存在，则本次发送失败；如果未关注，发送仍然执行。两种情况下均返回无效的部分（注：由于userid不区分大小写，返回的列表都统一转为小写</br> { "errcode":
 	 *         0, "errmsg": "ok", "invaliduser": "UserID1",
 	 *         "invalidparty":"PartyID1", "invalidtag":"TagID1" }
 	 * @throws WeixinException
