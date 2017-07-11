bee75e55cf5c408067329aedbc0116cc996d72c5
jinyu <foxinmy@gmail.com>
2017-07-11 17:13:07 +0800

    certificate file bug fixed

--- weixin4j-base/src/main/java/com/foxinmy/weixin4j/api/MchApi.java
+++ weixin4j-base/src/main/java/com/foxinmy/weixin4j/api/MchApi.java
@@ -3,6 +3,7 @@ package com.foxinmy.weixin4j.api;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -16,6 +17,7 @@ import com.foxinmy.weixin4j.sign.WeixinSignature;
 import com.foxinmy.weixin4j.type.IdQuery;
 import com.foxinmy.weixin4j.util.RandomUtil;
 import com.foxinmy.weixin4j.util.StringUtil;
+import com.foxinmy.weixin4j.util.Weixin4jConfigUtil;
 
 /**
  * 商户支付
@@ -28,101 +30,100 @@ import com.foxinmy.weixin4j.util.StringUtil;
  */
 public class MchApi extends BaseApi {
 
-	private final static ResourceBundle WEIXIN_BUNDLE;
+    private final static ResourceBundle WEIXIN_BUNDLE;
 
-	static {
-		WEIXIN_BUNDLE = ResourceBundle
-				.getBundle("com/foxinmy/weixin4j/payment/weixin");
-	}
+    static {
+        WEIXIN_BUNDLE = ResourceBundle.getBundle("com/foxinmy/weixin4j/payment/weixin");
+    }
 
-	protected final WeixinPayAccount weixinAccount;
-	protected final WeixinSignature weixinSignature;
-	private volatile WeixinRequestExecutor weixinSSLExecutor;
+    protected final WeixinPayAccount weixinAccount;
+    protected final WeixinSignature weixinSignature;
+    private volatile WeixinRequestExecutor weixinSSLExecutor;
 
-	public MchApi(WeixinPayAccount weixinAccount) {
-		this.weixinAccount = weixinAccount;
-		this.weixinSignature = new WeixinPaymentSignature(
-				weixinAccount.getPaySignKey());
-	}
+    public MchApi(WeixinPayAccount weixinAccount) {
+        this.weixinAccount = weixinAccount;
+        this.weixinSignature = new WeixinPaymentSignature(weixinAccount.getPaySignKey());
+    }
 
-	@Override
-	protected ResourceBundle weixinBundle() {
-		return WEIXIN_BUNDLE;
-	}
+    @Override
+    protected ResourceBundle weixinBundle() {
+        return WEIXIN_BUNDLE;
+    }
 
-	/**
-	 * 支付接口请求基本数据
-	 *
-	 * @param idQuery
-	 *            ID信息 可为空
-	 * @return 基础map
-	 */
-	protected Map<String, String> createBaseRequestMap(IdQuery idQuery) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("appid", weixinAccount.getId());
-		map.put("mch_id", weixinAccount.getMchId());
-		map.put("nonce_str", RandomUtil.generateString(16));
-		if (StringUtil.isNotBlank(weixinAccount.getDeviceInfo())) {
-			map.put("device_info", weixinAccount.getDeviceInfo());
-		}
-		if (StringUtil.isNotBlank(weixinAccount.getSubId())) {
-			map.put("sub_appid", weixinAccount.getSubId());
-		}
-		if (StringUtil.isNotBlank(weixinAccount.getSubMchId())) {
-			map.put("sub_mch_id", weixinAccount.getSubMchId());
-		}
-		if (idQuery != null) {
-			map.put(idQuery.getType().getName(), idQuery.getId());
-		}
-		return map;
-	}
+    /**
+     * 支付接口请求基本数据
+     *
+     * @param idQuery
+     *            ID信息 可为空
+     * @return 基础map
+     */
+    protected Map<String, String> createBaseRequestMap(IdQuery idQuery) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("appid", weixinAccount.getId());
+        map.put("mch_id", weixinAccount.getMchId());
+        map.put("nonce_str", RandomUtil.generateString(16));
+        if (StringUtil.isNotBlank(weixinAccount.getDeviceInfo())) {
+            map.put("device_info", weixinAccount.getDeviceInfo());
+        }
+        if (StringUtil.isNotBlank(weixinAccount.getSubId())) {
+            map.put("sub_appid", weixinAccount.getSubId());
+        }
+        if (StringUtil.isNotBlank(weixinAccount.getSubMchId())) {
+            map.put("sub_mch_id", weixinAccount.getSubMchId());
+        }
+        if (idQuery != null) {
+            map.put(idQuery.getType().getName(), idQuery.getId());
+        }
+        return map;
+    }
 
-	/**
-	 * 微信签名类
-	 *
-	 * @return
-	 */
-	public WeixinSignature getWeixinSignature() {
-		return this.weixinSignature;
-	}
+    /**
+     * 微信签名类
+     *
+     * @return
+     */
+    public WeixinSignature getWeixinSignature() {
+        return this.weixinSignature;
+    }
 
-	/**
-	 * 微信SSL
-	 * 
-	 * @return
-	 */
-	protected WeixinRequestExecutor getWeixinSSLExecutor()
-			throws WeixinException {
-		if (weixinSSLExecutor == null) {
-			try {
-				File certificate = new File(weixinAccount.getCertificateFile());
-				if (!certificate.exists() || !certificate.isFile()) {
-					throw new WeixinException("Invalid certificate file : "
-							+ certificate.toString());
-				}
-				this.weixinSSLExecutor = weixinExecutor
-						.createSSLRequestExecutor(
-								weixinAccount.getCertificateKey(),
-								new FileInputStream(certificate));
-			} catch (IOException e) {
-				throw new WeixinException(
-						"IO Error on createSSLRequestExecutor", e);
-			}
-		}
-		return this.weixinSSLExecutor;
-	}
+    /**
+     * 微信SSL
+     *
+     * @return
+     */
+    protected WeixinRequestExecutor getWeixinSSLExecutor() throws WeixinException {
+        if (weixinSSLExecutor == null) {
+            try {
+                InputStream is = null;
+                File certificate = new File(
+                        Weixin4jConfigUtil.replaceClassPathValue(weixinAccount.getCertificateFile()));
+                if (!certificate.exists() || !certificate.isFile()) {
+                    is = Weixin4jConfigUtil.CLASSLOADER.getResourceAsStream(weixinAccount.getCertificateFile());
+                } else {
+                    is = new FileInputStream(certificate);
+                }
+                if (is == null) {
+                    throw new WeixinException("Invalid certificate file : " + certificate.toString());
+                }
+                this.weixinSSLExecutor = weixinExecutor.createSSLRequestExecutor(weixinAccount.getCertificateKey(), is);
+            } catch (IOException e) {
+                throw new WeixinException("IO Error on createSSLRequestExecutor", e);
+            }
+        }
+        return this.weixinSSLExecutor;
+    }
 
-	/**
-	 * 设置商户信息
-	 *
-	 * @param merchant
-	 */
-	protected <T extends MerchantResult> void declareMerchant(T merchant) {
-		merchant.setAppId(weixinAccount.getId());
-		merchant.setMchId(weixinAccount.getMchId());
-		merchant.setDeviceInfo(weixinAccount.getDeviceInfo());
-		merchant.setSubAppId(weixinAccount.getSubId());
-		merchant.setSubMchId(weixinAccount.getSubMchId());
-		merchant.setNonceStr(RandomUtil.generateString(16));
-	}
+    /**
+     * 设置商户信息
+     *
+     * @param merchant
+     */
+    protected <T extends MerchantResult> void declareMerchant(T merchant) {
+        merchant.setAppId(weixinAccount.getId());
+        merchant.setMchId(weixinAccount.getMchId());
+        merchant.setDeviceInfo(weixinAccount.getDeviceInfo());
+        merchant.setSubAppId(weixinAccount.getSubId());
+        merchant.setSubMchId(weixinAccount.getSubMchId());
+        merchant.setNonceStr(RandomUtil.generateString(16));
+    }
 }
