d3044f045d47d8d19a3194e3373f2e67fe020e9b
Niko <1377382065@qq.com>
2020-01-07 10:13:52 +0800

    SubscribeMessageParameter public to package

--- weixin4j-wxa/src/main/java/com/foxinmy/weixin4j/wxa/api/SubscribeMessageParameter.java
+++ weixin4j-wxa/src/main/java/com/foxinmy/weixin4j/wxa/api/SubscribeMessageParameter.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SubscribeMessageParameter {
+class SubscribeMessageParameter {
 
 	private static final long serialVersionUID = 2018052601L;
 
